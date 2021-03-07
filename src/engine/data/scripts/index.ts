8b1e487a92b814d71348105ec846107b0b68bd2a
NonSpicyBurrito <burritoyangster@gmail.com>
2021-03-07 08:01:18 +0800

    Initial commit

---
+++ src/engine/data/scripts/index.ts
@@ -0,0 +1,9 @@
+import { defineScripts } from 'sonolus.js'
+
+import { initialization } from './initialization'
+import { stage } from './stage'
+
+export const scripts = defineScripts({
+    initialization,
+    stage,
+})
