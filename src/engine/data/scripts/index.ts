3d3cf57c2cfde22aa78c247ce50323110fefb717
NonSpicyBurrito <burritoyangster@gmail.com>
2021-03-07 08:01:18 +0800

    Initial commit

---
+++ src/engine/data/scripts/index.ts
@@ -0,0 +1,7 @@
+import { defineScripts } from 'sonolus.js'
+
+import { initialization } from './initialization'
+
+export const scripts = defineScripts({
+    initialization,
+})
---
+++ src/engine/data/scripts/initialization.ts
@@ -0,0 +1,41 @@
+import {
+    DebugLog,
+    HorizontalAlign,
+    ScreenAspectRatio,
+    SScript,
+    Subtract,
+    UIMenu,
+    VerticalAlign,
+} from 'sonolus.js'
+
+export function initialization(): SScript {
+    const preprocess = UIMenu.set(
+        Subtract(0.05, ScreenAspectRatio),
+        0.95,
+        0,
+        1,
+        0.15,
+        0.15,
+        0,
+        1,
+        HorizontalAlign.Center,
+        VerticalAlign.Middle,
+        true
+    )
+
+    const spawnOrder = 0
+
+    const updateSequential = [DebugLog(42), true]
+
+    return {
+        preprocess: {
+            code: preprocess,
+        },
+        spawnOrder: {
+            code: spawnOrder,
+        },
+        updateSequential: {
+            code: updateSequential,
+        },
+    }
+}
